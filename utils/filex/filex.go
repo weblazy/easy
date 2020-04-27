3d9f59a03747dd8891965224776849a45cfc8f3e
liuguoqiang <liuguoqiang@xiaoheiban.cn>
2020-04-27 16:55:45 +0800

    feat:file

---
+++ utils/filex/filex.go
@@ -0,0 +1,19 @@
+package filex
+
+import (
+	"io/ioutil"
+	"os"
+)
+
+func ReadFile(path string) ([]byte, error) {
+	fi, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer fi.Close()
+	fd, err := ioutil.ReadAll(fi)
+	if err != nil {
+		return nil, err
+	}
+	return fd, nil
+}
