c17317ea18271dee7174a77a30d0b51a7eadd57a
lazy <2276282419@qq.com>
2021-12-10 09:16:34 +0800

    feat: http

--- go.mod
+++ go.mod
@@ -3,10 +3,13 @@ module github.com/weblazy/easy
 go 1.12
 
 require (
+	github.com/go-resty/resty/v2 v2.6.0
 	github.com/jinzhu/gorm v1.9.16
 	github.com/mattn/go-sqlite3 v2.0.1+incompatible // indirect
+	github.com/spf13/cast v1.4.1
 	github.com/stretchr/testify v1.7.0
 	github.com/sunmi-OS/gocore v1.5.10
+	github.com/sunmi-OS/gocore/v2 v2.0.9
 	github.com/tal-tech/go-zero v1.2.2
 	github.com/weblazy/gocore v1.7.2
 	github.com/weblazy/goutil v1.1.2
---
+++ utils/httpx/log.go
@@ -0,0 +1,91 @@
+package httpx
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/spf13/cast"
+	"github.com/sunmi-OS/gocore/v2/glog"
+	"github.com/sunmi-OS/gocore/v2/glog/sls"
+)
+
+type Log interface {
+	Info(obj *LogObject) error
+}
+
+type LogObject struct {
+	Url             string        `json:"url"`
+	Method          string        `json:"method"`
+	RequestHders    http.Header   `json:"request_headers"`
+	RequestRawBody  interface{}   `json:"request_raw_body"`
+	ResponseHeaders http.Header   `json:"response_headers"`
+	ResponseBody    string        `json:"response_body"`
+	StartTime       string        `json:"start_time"`
+	Duration        time.Duration `json:"duration"`
+	Status          int           `json:"status"`
+}
+
+func (h *HttpClient) SetLog(log Log) *HttpClient {
+	err := h.Client.OnAfterResponse(func(client *resty.Client, resp *resty.Response) error {
+		r := resp.Request
+		obj := &LogObject{
+			Url:             r.URL,
+			Method:          r.Method,
+			RequestHders:    r.Header,
+			RequestRawBody:  r.Body,
+			ResponseHeaders: resp.Header(),
+			ResponseBody:    string(resp.Body()),
+			StartTime:       r.Time.Format("2006-01-02 15:04:05"),
+			Duration:        resp.Time() / time.Millisecond,
+			Status:          resp.StatusCode(),
+		}
+		log.Info(obj)
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("%#v\n", err)
+	}
+	return h
+}
+
+type GocoreLog struct {
+}
+
+func NewGocoreLog() *GocoreLog {
+	return &GocoreLog{}
+}
+
+func (l *GocoreLog) Info(obj *LogObject) error {
+	data, _ := json.Marshal(obj)
+	glog.InfoF(string(data))
+	return nil
+}
+
+func NewAliyunLog(topic string) *AliyunLog {
+	return &AliyunLog{topic: topic}
+}
+
+type AliyunLog struct {
+	topic string
+}
+
+//  使用阿里云日志需要提前调用sls.InitLog初始化
+func (l *AliyunLog) Info(obj *LogObject) error {
+	requestHeaderBytes, _ := json.Marshal(obj.RequestHders)
+	requestBodyBytes, _ := json.Marshal(obj.RequestRawBody)
+	responseHeaderBytes, _ := json.Marshal(obj.ResponseHeaders)
+	_ = sls.Info(l.topic, map[string]string{
+		"url":              obj.Url,
+		"method":           obj.Method,
+		"request_headers":  string(requestHeaderBytes),
+		"request_raw_body": string(requestBodyBytes),
+		"response_headers": string(responseHeaderBytes),
+		"start_time":       obj.StartTime,
+		"duration":         cast.ToString(obj.Duration),
+		"status":           cast.ToString(obj.Status),
+	})
+	return nil
+}
