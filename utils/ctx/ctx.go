2cc86c8d7ca7d559b1f4751512fc3824734d5bd4
lazy <2276282419@qq.com>
2022-05-31 16:34:55 +0800

    feat: ctx

---
+++ utils/ctx/ctx.go
@@ -0,0 +1,21 @@
+package ctx
+
+import (
+	"context"
+	"time"
+)
+
+// NoCancelContext remove context deadline
+type NoCancelContext struct {
+	ctx context.Context
+}
+
+func (c NoCancelContext) Deadline() (time.Time, bool)       { return time.Time{}, false }
+func (c NoCancelContext) Done() <-chan struct{}             { return nil }
+func (c NoCancelContext) Err() error                        { return nil }
+func (c NoCancelContext) Value(key interface{}) interface{} { return c.ctx.Value(key) }
+
+// NoCancel remove ctx deadline then return a new context
+func NoCancel(ctx context.Context) context.Context {
+	return NoCancelContext{ctx}
+}
