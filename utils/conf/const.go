380024614c122e74482af1611b3d10abd0f9e086
lazy <2276282419@qq.com>
2022-11-16 15:19:29 +0800

    feat: config

--- utils/conf/const.go
+++
@@ -1,5 +0,0 @@
-package conf
-
-const (
-	EASY_CONFIG_TYPY = "EASY_CONFIG_TYPY"
-)
---
+++ utils/econfig/const.go
@@ -0,0 +1,9 @@
+package econfig
+
+const (
+	EasyConfigType  = "EASY_CONFIG_TYPE"
+	EasyConfigTFile = "EASY_CONFIG_FILE"
+	LocalType       = "local"
+	FielType        = "file"
+	NacosType       = "nacos"
+)
