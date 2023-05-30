4440f5e29168cdefe5d6e0a1ef63e97aec177f6d
lazy <2276282419@qq.com>
2023-05-30 11:11:20 +0800

    feat: sort

--- sortx/sort.go
+++ sortx/sort.go
@@ -1,22 +1,42 @@
 package sortx
 
-//按照接口中的某个字段排序
+// 按照接口中的某个字段排序
+type Order string
+
+const (
+	ASC  Order = "ASC"
+	DESC Order = "DESC"
+)
+
 type Sort struct {
 	Obj  interface{} `json:"obj"`
 	Sort int64       `json:"sort"`
 }
-type SortList []Sort
-
-func (list SortList) Len() int {
-	return len(list)
+type SortList struct {
+	List  []Sort
+	Order Order
 }
 
-func (list SortList) Less(i, j int) bool {
-	return list[i].Sort <= list[j].Sort
+func NewSortList(order Order) *SortList {
+	return &SortList{
+		List:  make([]Sort, 0),
+		Order: order,
+	}
 }
 
-func (list SortList) Swap(i, j int) {
-	temp := list[i]
-	list[i] = list[j]
-	list[j] = temp
+func (list *SortList) Len() int {
+	return len(list.List)
+}
+
+func (list *SortList) Less(i, j int) bool {
+	if list.Order == ASC {
+		return list.List[i].Sort <= list.List[j].Sort
+	}
+	return list.List[i].Sort >= list.List[j].Sort
+}
+
+func (list *SortList) Swap(i, j int) {
+	temp := list.List[i]
+	list.List[i] = list.List[j]
+	list.List[j] = temp
 }
