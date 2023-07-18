9e736a2dd727bc0055c423e4e1731f837113fa2b
lazy <2276282419@qq.com>
2023-07-18 10:58:03 +0800

    feat: token

--- http/http_server/interceptor/token.go
+++ http/http_server/interceptor/token.go
@@ -7,13 +7,13 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/weblazy/easy/code_err"
-	"github.com/weblazy/easy/env"
+	"github.com/weblazy/easy/econfig"
 )
 
 // Token
 func Token(c *gin.Context) {
 	debugKey := c.Request.Header.Get(DebugHeader)
-	if env.GetRunTime() == "onl" || debugKey != "test" {
+	if !econfig.GlobalViper.GetBool("BaseConfig.Debug") || debugKey == econfig.GlobalViper.GetString("BaseConfig.XDebugKey") {
 		token := c.Request.Header.Get(TokenHeader)
 		if token == "" {
 			Error(c, code_err.TokenErr, fmt.Errorf("token 不存在"))
@@ -37,7 +37,7 @@ func Sign(validateToken func(token string) (uid string, err error)) gin.HandlerF
 		}
 		// 新建缓冲区并替换原有Request.body
 		c.Request.Body = ioutil.NopCloser(bytes.NewBuffer([]byte(bodyBytes)))
-		if env.GetRunTime() == "onl" || debugKey != "test" {
+		if !econfig.GlobalViper.GetBool("BaseConfig.Debug") || debugKey == econfig.GlobalViper.GetString("BaseConfig.XDebugKey") {
 			sign := header.Get(SignHeader)
 			token := header.Get(TokenHeader)
 			timestamp := header.Get(TimestampHeader)
