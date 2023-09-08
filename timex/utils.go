cf13871b1b90032e5abfc4d2bcf27a13be2a7360
lazy <2276282419@qq.com>
2023-09-08 10:33:06 +0800

    feat: time

--- timex/utils.go
+++ timex/utils.go
@@ -15,6 +15,10 @@ func ZeroTime(d time.Time) time.Time {
 	return time.Date(d.Year(), d.Month(), d.Day(), 0, 0, 0, 0, time.Local)
 }
 
+func ZeroTimeWithLocation(d time.Time, loc *time.Location) time.Time {
+	return time.Date(d.Year(), d.Month(), d.Day(), 0, 0, 0, 0, loc)
+}
+
 // @desc 返回一个月的开始时间和结束时间
 // @auth liuguoqiang 2020-04-27
 // @param
@@ -23,6 +27,14 @@ func MonthRange(timeStamp int64) (int64, int64) {
 	d := time.Unix(timeStamp, 0)
 	d = d.AddDate(0, 0, -d.Day()+1)
 	start := ZeroTime(d)
+	end := start.AddDate(0, 1, 0)
+	return start.Unix(), end.Unix()
+}
+
+func MonthRangeWthiLocation(timeStamp int64, loc *time.Location) (int64, int64) {
+	d := time.Unix(timeStamp, 0)
+	d = d.AddDate(0, 0, -d.Day()+1)
+	start := ZeroTimeWithLocation(d, loc)
 	end := start.AddDate(0, 1, 0)
 	return start.Unix(), end.Unix()
 }
