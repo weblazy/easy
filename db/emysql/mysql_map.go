557a381054b1637556904d01f2a8525ed26b3e1c
lazy <2276282419@qq.com>
2025-11-03 15:17:46 +0800

    transaction
    Refactor GetMysql function to accept context and return *gorm.DB. Update getMysql to use context for database operations.

--- db/emysql/mysql_map.go
+++ db/emysql/mysql_map.go
@@ -1,18 +1,30 @@
 package emysql
 
 import (
+	"context"
 	"sync"
 
 	"github.com/weblazy/easy/db/emysql/emysql_config"
 	"github.com/weblazy/easy/econfig"
+	"gorm.io/gorm"
 )
 
 var MysqlMap sync.Map
 
 // GetMysql return a MysqlClient
-func GetMysql(dbName string) *MysqlClient {
+func GetMysql(ctx context.Context, dbName string) *gorm.DB {
+	// 判断是否开启了事务，如果存在dbname开启事务直接返回/如果没有开启并启动事务
+	tx := checkTransaction(ctx, dbName)
+	if tx != nil {
+		return tx
+	}
+	return getMysql(ctx, dbName)
+}
+
+// getMysql return a *gorm.DB
+func getMysql(ctx context.Context, dbName string) *gorm.DB {
 	if v, ok := MysqlMap.Load(dbName); ok {
-		return v.(*MysqlClient)
+		return v.(*MysqlClient).DB.WithContext(ctx)
 	}
 	conf := emysql_config.DefaultConfig()
 	econfig.GlobalViper.UnmarshalKey(dbName, conf)
@@ -20,6 +32,7 @@ func GetMysql(dbName string) *MysqlClient {
 	if err != nil {
 		return nil
 	}
+
 	MysqlMap.Store(dbName, mysqlClient)
-	return mysqlClient
+	return mysqlClient.DB.WithContext(ctx)
 }
