342e43f5c92be781783cbaafc66289e3d4898923
lazy <2276282419@qq.com>
2025-11-04 19:01:17 +0800

    Implement PrintHint for colored out
    Add PrintHint function to print colored hints

--- print/print.go
+++ print/print.go
@@ -4,10 +4,19 @@ import (
 	"fmt"
 
 	"github.com/common-nighthawk/go-figure"
+	"github.com/fatih/color"
 )
 
 func PrintBanner(name string) {
 	myFigure := figure.NewFigure(name, "", true)
 	myFigure.Print()
 	fmt.Println()
+}
+
+// PrintHint 打印提示
+func PrintHint(str string) {
+	_, err := color.New(color.FgCyan, color.Bold).Print(str + "\n")
+	if err != nil {
+		return
+	}
 }
